515ec541cb770df9e9589b545276e1954bc48b22
Ayan <sujon.seo.bd@gmail.com>
2025-09-24 21:13:53 +0600

    added css class

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-    <h1 className='color-acua'>hello world</h1>
+    <h1 className='color-aqua'>hello world</h1>
 <div className="navbar bg-base-100 shadow-sm">
   <div className="flex-1">
     <a className="btn btn-ghost text-xl">daisyUI</a>
