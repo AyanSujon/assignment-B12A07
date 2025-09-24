6d018b6c4ff456af27774e1b53e550aeb4edfe3b
Ayan <sujon.seo.bd@gmail.com>
2025-09-24 21:09:41 +0600

    Just added resources

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='color-acua'>hello world</h1>
+<div className="navbar bg-base-100 shadow-sm">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Home</a></li>
+      <li><a>FAQ</a></li>
+      <li><a>Changelog</a></li>
+      <li><a>Blog</a></li>
+      <li><a>Download</a></li>
+      <li><a>Contact</a></li>
+      <li><a className='btn-gradient font'><span>+</span> New Ticket</a></li>
+    </ul>
+  </div>
+</div>
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
