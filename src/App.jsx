acc70e6cd2755787270703a8ce75d1d993d2a843
Ayan <sujon.seo.bd@gmail.com>
2025-09-24 21:51:40 +0600

    added custom css

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,39 @@ function App() {
 
   return (
     <>
-    <h1 className='color-aqua'>hello world</h1>
+
 <div className="navbar bg-base-100 shadow-sm">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow  ">
       <li><a>Home</a></li>
       <li><a>FAQ</a></li>
       <li><a>Changelog</a></li>
       <li><a>Blog</a></li>
       <li><a>Download</a></li>
       <li><a>Contact</a></li>
-      <li><a className='btn-gradient font'><span>+</span> New Ticket</a></li>
+      <li><a className='btn-gradient text-white'><span>+</span> New Ticket</a></li>
+      </ul>
+    </div>
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="navbar-end hidden lg:flex">
+    <ul className="menu menu-horizontal px-0.5">
+      <li><a>Home</a></li>
+      <li><a>FAQ</a></li>
+      <li><a>Changelog</a></li>
+      <li><a className='hero-gradient-left'>Blog</a></li>
+      <li><a className='hero-gradient-right'>Download</a></li>
+      <li><a>Contact</a></li>
+      <li><a className='btn-gradient text-white'><span>+</span> New Ticket</a></li>
     </ul>
   </div>
 </div>
-
-
 
 
 
