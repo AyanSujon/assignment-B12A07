33f184c3d7b88511164f781ab28139d54d468bcd
Ayan Sujon <ayansujonbd@gmail.com>
2025-09-25 02:39:44 +0600

    TaskStatus component added

--- src/components/mainSection/CustomerTickets.jsx
+++ src/components/mainSection/CustomerTickets.jsx
@@ -3,7 +3,20 @@ import React from 'react';
 const CustomerTickets = () => {
     return (
         <div className='col-span-12 md:col-span-9 h-100 border-2 border-amber-100'>
-            <h1>Customer Tickets</h1>
+            <h2 className='mb-3 font-semibold text-2xl text-[#34485A]'>Task Status</h2>
+            <div id='customer-tickets-container'>
+                <div className="customer-tickets-card">
+                    <div className="card card-dash bg-base-100 w-96">
+                        <div className="card-body">
+                            <h2 className="card-title">Card Title</h2>
+                            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                            <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/mainSection/TaskStatus.jsx
+++ src/components/mainSection/TaskStatus.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const TaskStatus = () => {
     return (
         <div className=' col-span-12 md:col-span-3  h-100 border-2 border-red-100'>
-            <div>
-                <h1 className='mb-3 font-semibold text-2xl text-[#34485A]'>Task Status</h1>
+            <div className='task-status-container'>
+                <h2 className='mb-3 font-semibold text-2xl text-[#34485A]'>Task Status</h2>
                 <div id='task-status-card' className="space-y-3">
                     <p className='text-[#627382]'>Select a ticket to add to Task Status</p>
                     <div class="card bg-base-100 card-xs shadow-sm">
@@ -17,7 +17,18 @@ const TaskStatus = () => {
                     </div>
                 </div>
             </div>
-
+            {/* Resolved Task */}
+                <div className='resolved-task-container'>
+                <h2 className='mb-3 font-semibold text-2xl text-[#34485A]'>Resolved Task</h2>
+                <div id='resolved-task-card' className="space-y-3">
+                    <p className='text-[#627382]'>No resolved tasks yet.</p>
+                    <div class="card bg-[#E0E7FF] card-xs shadow-sm">
+                    <div class="card-body">
+                        <h2 class="card-title">Incorrect Billing Address</h2>
+                    </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
